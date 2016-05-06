89b0a7f34662472ad1ffae3220b9dbac6dc66ef5
adityakumarakash <adityakumarakash@gmail.com>
2016-05-06 22:56:26 +0530

    train based on selective instance added

---
+++ matlabExperiments/Code/experimentCase1InstLabel.m
@@ -0,0 +1,147 @@
+%% This script is for experiment of case 1
+% The models are trained on different dataset. MLCM identifies which models
+% need to be retrained, thus updating the training set of the models
+
+%% setup configuration
+N = 10;                     % N models are created
+DatasetName = 'medical';
+k = 5;                     % k fold CV is done for training the models
+expNum = 1;
+libSVMPath = '../../libsvm-3.21/matlab';
+config.libSVMPath = libSVMPath;
+addpath(libSVMPath);
+fileName = '../Output/temp_expCase1.txt';
+fId = fopen(fileName, 'a');
+
+fprintf(fId, '\n--------------------------------------------------\n');
+fprintf(fId, 'Dataset %s\n', DatasetName);
+%% loading the data from the dataset
+[trainData, trainLabel, testData, testLabel] = loadDataset(DatasetName);
+OL = testLabel;
+OL(OL==0) = -1;
+% trainData = trainData(100 : 200, :);
+% testData = testData(100 : 200, :);
+% trainLabel = trainLabel(100 : 200, :);
+% testLabel = testLabel(100 : 200, :);
+
+%% Partition data for training each of the models
+trainDataModels = cell(N, 1);
+trainLabelModels = cell(N, 1);
+trainIndexLabels = cell(N, 1);
+fraction = 0.9;
+trainInst = int32(size(trainData, 1) * 0.9);
+totalInstance = size(trainData, 1);
+parfor i = 1 : N
+    index = randsample(totalInstance, trainInst);
+    trainDataModels{i} = trainData(index, :);
+    trainLabelModels{i} = trainLabel(index, :);
+    trainIndexLabels{i} = ones(size(trainLabelModels{i}));  % constructing an index
+end
+fprintf(fId, 'Training Data generated\n');
+maxIteration = 2;
+agreementMatrix = zeros(N, maxIteration);
+instanceAgreement = zeros(size(testData, 1), maxIteration);
+
+for iteration = 1 : maxIteration
+    %% train each model
+    modelSet = cell(N, 1);
+    parfor i = 1 : N    
+        M = trainModelUsingCVSelectiveOnLabel(trainDataModels{i}, trainLabelModels{i}, trainIndexLabels{i}, k);
+        modelSet{i} = M;
+    end
+    fprintf(fId, 'Models trained\n');
+
+    %% prediction from each model
+    P = zeros(size(testData, 1), size(testLabel, 2) * N);
+    labelCount = size(testLabel, 2);
+    for i = 1 : N
+        P(:, (i - 1) * labelCount + 1 : i * labelCount) = predictLabels(modelSet{i}, testData);
+    end
+
+    fprintf(fId, 'Prediction Done\n');
+
+    %% MLCM on the prediction
+    index = sum(P, 2) ~= 0;
+    P = P(index, :);
+    OL = testLabel;
+    OL(OL==0) = -1;
+    OL = OL(index, :);
+    A = P;
+    P(P == 0) = -1;
+    nInstances = size(A, 1);
+    nClasses = size(testLabel, 2);
+    nModels = N;
+    alpha = 1;
+
+    % label matrix for group nodes
+    L = eye(nClasses);
+    B = repmat(L, nModels, 1);
+    [U, Q] = MLCMrClosedForm(nInstances, nClasses, nModels, A, alpha, B);
+    fprintf(fId, 'MLCM prediction done\n');
+
+    L = binarizeProbDist(U, P);
+    fprintf(fId, 'Binarization Done\n');
+    labelPredictions = L;
+    labelPredictions(L==-1) = 0;
+    % get the agreement values of the instances
+    K = zeros(nInstances, 1);
+    parfor i = 1 : nInstances
+        K(i) = findAgreement(L(i, :), P(i, :));
+    end
+
+
+    fScore = findFScore2(L, OL, 1);
+    fprintf(fId, 'f score is %f\n', fScore);
+
+
+%     % find predictions
+%     userConfidenceMicro = zeros(nModels, 1);
+%     for i = 1 : nModels
+%         userConfidenceMicro(i) = findKappaUser(L, P(:, (i - 1) * nClasses + 1 : i * nClasses));
+%     end
+% 
+    userConfidenceMacro = zeros(nModels, 1);
+    for i = 1 : nModels
+        userConfidenceMacro(i) = findUserConfidenceMacro(L, P(:, (i - 1) * nClasses + 1 : i * nClasses));
+    end
+% 
+%     userConfidenceMean = zeros(nModels, 1);
+%     LRep = repmat(L, 1, nModels);
+%     K = findKappaUserLabel(P, LRep);          % kappa values for each user , label
+%     for i = 1 : nModels
+%         temp = K((i - 1) * nClasses + 1 : i * nClasses);
+%         userConfidenceMean(i) = sum(temp) / sum(temp~=-2);
+%     end
+% 
+    userCapacity = userConfidenceMacro;
+    agreementMatrix(:, iteration) = userCapacity;
+
+    instanceAgreement(:, iteration) = K;
+
+    if iteration > 1
+        fprintf(fId, 'Average Change in agreement = %f\n', sum(instanceAgreement(:, iteration) - instanceAgreement(:, iteration - 1)));
+        fprintf(fId, 'Average Change in agreement User = %f\n', sum(agreementMatrix(:, iteration) - agreementMatrix(:, iteration - 1)));
+    end
+    %histogram(K);
+    lthreshold = 0.85; rthreshold = 0.95;
+    improvementSet = (K >= lthreshold).*(K<rthreshold);
+    fprintf(fId, '%d instances\n', sum(improvementSet));
+    improvementSet = cumsum(improvementSet).*improvementSet;
+    improvementSet = improvementSet(improvementSet ~= 0);
+    count = size(improvementSet, 1);
+    for i = 1 : count
+        inst = improvementSet(i);
+        modelNum = 3;
+        disModel = getDisagreementModels(L(inst, :), P(inst, :), modelNum);
+        % appending the new instances
+        for j = 1 : modelNum
+            m = disModel(j);
+            pred = P(inst, (m - 1) * nClasses + 1, m * nClasses);
+            trainDataModels{m} = [trainDataModels{m}; testData(inst, :)];
+            % consensus output is given as GT
+            trainLabelModels{m} = [trainLabelModels{m}; labelPredictions(inst, :)]; 
+            trainIndexLabels{m} = [trainIndexLabels{m}; labelPredictions(inst, :) ~= pred];
+        end
+    end
+
+end
---
+++ matlabExperiments/Code/trainModelUsingCVSelectiveOnLabel.m
@@ -0,0 +1,86 @@
+function [ modelMatrix ] = trainModelUsingCVSelectiveOnLabel( trainData, trainLabel, labelIndex, CVFold )
+%   Returns a matrix of SVM models for each of the labels
+%   Detailed explanation goes here
+
+%% Parameters
+k = CVFold;    
+beta = 1;
+
+data = trainData;
+label = trainLabel;
+
+%% get N number of models / predictions
+instanceCount = size(trainData, 1); % instance count in training
+labelCount = size(trainLabel, 2);   % labels count 
+
+%% select CV folds
+CV = cvpartition(instanceCount, 'KFold', k);
+
+%% Train SVM on each partition and each label
+CArr = zeros(labelCount, 1);
+GammaArr = zeros(labelCount, 1);
+featureCount = size(trainData, 2);
+
+for l = 1 : labelCount
+    CArr(l) = -1; GammaArr(l) = -5; fMax = 0;
+    lowC = 4; highC = 8;
+    lowG = 2 - log2(featureCount); highG = 3 - log2(featureCount);
+    
+    trainData = data(labelIndex(l), :);
+    trainLabel = label(labelIndex(l), :);
+    
+    for c = lowC : highC
+        for g = lowG : highG
+            config.C = c;
+            config.gamma = g;
+            fAvg = 0;
+            parfor i = 1 : k
+                trainDataCV = trainData(CV.training(i), :);
+                trainLabelCV = trainLabel(CV.training(i), l);
+                testDataCV = trainData(CV.test(i), :);
+                testLabelCV = trainLabel(CV.test(i), l);
+
+                % add non zero row to training data
+                nzRow = find(trainLabelCV, 1);
+
+                % train SVM
+                if size(nzRow, 1) * size(nzRow, 2) == 0
+                    nzRow = 1;
+                end
+                model = trainSVM([trainDataCV(nzRow, :); trainDataCV], [1; trainLabelCV], config);
+
+                % prediction for training data
+                %[predictionLabelTrn, accuracyTrn, ~] = svmpredict(trainLabelCV, trainDataCV, model);
+                [predictionLabelCV, ~, ~] = svmpredict(testLabelCV, testDataCV, model);
+                f = findFScore(predictionLabelCV, testLabelCV, beta);
+                fAvg = fAvg + f;
+            end
+            
+            fAvg = fAvg / k;
+            if fAvg > fMax
+                fMax = fAvg;
+                CArr(l) = config.C;
+                GammaArr(l) = config.gamma;
+            end
+        end
+    end
+
+    % add non zero row to training data
+    nzRow = find(trainLabel(:, l), 1);
+    
+    if size(nzRow, 1) * size(nzRow, 2) == 0
+        nzRow = 1;
+    end
+    % train SVM on the optimum parameters
+    config.C = CArr(l);
+    config.gamma = GammaArr(l);
+    model = trainSVM([trainData(nzRow, :); trainData], [1; trainLabel(:, l)], config);   % train the model on the entire data
+    if l == 1
+        modelMatrix = model;
+    else
+        modelMatrix = [modelMatrix model];  % append each label model to the matrix
+    end
+end
+
+end
+
